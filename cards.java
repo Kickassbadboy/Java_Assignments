86ff16cdca15f90ce5be1bbaa04c78d78b7b63d2
Yash Talegaonkar <yash.talegaonkar37@gmail.com>
2018-10-01 00:39:32 +0530

    new file structure for better understanding

--- cards.java
+++ cards.java
@@ -1,33 +1,33 @@
 
-public abstract class Cards {
-	
+public abstract class cards {
+
 	private String Subject;
 	private String Question;
 	private String Answer;
-	
-	public String getSubject() 
+
+	public String getSubject()
 	{
 	      return this.Subject;
 	}
- 
-	
+
+
 	public String getQuestion()
 	{
-		return  this.Question; 
+		return  this.Question;
 	}
-	
+
 	public String getAnswer()
 	{
 		return this.Answer;
 	}
-	   
-	
-	
+
+
+
 	public void setSubject( String newSubject) {
 	      Subject = newSubject;
 	   }
-	
-	
+
+
 	public void setQuestion(String newQuestion) {
 		Question=newQuestion;
 	}
@@ -35,15 +35,24 @@ public abstract class Cards {
 	public void setAnswer(String newAnswer) {
 		Answer=newAnswer;
 	}
-	
 
-	public Cards() {
+
+	public cards() {
 		// TODO Auto-generated constructor stub
-		
+
 	}
+
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+    // TODO Created fake data insertion
+
+		cards=new cards();
+		cards.setSubject("Java");
+		cards.setQuestion("What is this in Java");
+		cards.setAnswer("this is a keyword in Java ");
 
 	}
 
---
+++ flashCard.java
@@ -0,0 +1,31 @@
+
+
+public class flashCard extends cards{
+
+  //TODO Created a parameterized constructor for entering values into the cards Data.
+  
+  flashCard()
+  {
+
+  }
+}
+
+
+public class flashCardData extends flashCard implements cardSearchable{
+
+
+
+}
+
+
+
+
+// TODO Created a main class for this file
+public class main3{
+public static void main(String args[])
+{
+
+
+}
+
+}
